7066391933196eabe3e9894c7bd31164cdb16472
Lekesh Kumar <39632067+lekesh@users.noreply.github.com>
2022-04-12 11:42:13 +0530

    Add files via upload

---
+++ python_slack.py
@@ -0,0 +1,31 @@
+import json
+import sys
+import random
+import requests
+
+if __name__ == '__main__':
+    url = "<WebHook_URL>"
+    message = ("A Sample Message")
+    title = (f"New Incoming Message :bowtie:")
+    slack_data = {
+        "username": "NotificationBot",
+        "icon_emoji": ":satellite:",
+        #"channel" : "#somerandomcahnnel",
+        "attachments": [
+            {
+                "color": "#9733EE",
+                "fields": [
+                    {
+                        "title": title,
+                        "value": message,
+                        "short": "false",
+                    }
+                ]
+            }
+        ]
+    }
+    byte_length = str(sys.getsizeof(slack_data))
+    headers = {'Content-Type': "application/json", 'Content-Length': byte_length}
+    response = requests.post(url, data=json.dumps(slack_data), headers=headers)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
